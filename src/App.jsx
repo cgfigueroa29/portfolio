eace7a6819a54fcd732d2d3efe668319666144c3
Camilo Figueroa <cgfigueroa29@gmail.com>
2025-11-30 23:08:57 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Technologies from "./components/Technologies";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Education from "./components/Education";
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<div class="content">
+				<About />
+				<Technologies />
+				<Experience />
+				<Projects />
+				<Education />
+			</div>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Technologies.jsx
@@ -0,0 +1,52 @@
+import react from "../images/react.png";
+import node from "../images/node.png";
+import github from "../images/github.png";
+import git from "../images/git.png";
+import js from "../images/js.png";
+import css from "../images/css.png";
+import html from "../images/html.png";
+import mysql from "../images/mysql.png";
+
+export default function Technologies() {
+	return (
+		<>
+			<section id="technologies">
+				<h3>Technologies</h3>
+				<div class="technologies-container">
+					<div class="technologies-item">
+						<img class="item-img" src={react} alt="" />
+						React.js
+					</div>
+					<div class="technologies-item">
+						<img class="item-img" src={node} alt="" />
+						Node.js
+					</div>
+					<div class="technologies-item">
+						<img class="item-img" src={github} alt="" />
+						GitHub
+					</div>
+					<div class="technologies-item">
+						<img class="item-img" src={git} alt="" />
+						Git
+					</div>
+					<div class="technologies-item">
+						<img class="item-img" src={js} alt="" />
+						JS
+					</div>
+					<div class="technologies-item">
+						<img class="item-img" src={css} alt="" />
+						CSS
+					</div>
+					<div class="technologies-item">
+						<img class="item-img" src={html} alt="" />
+						HTML
+					</div>
+					<div class="technologies-item">
+						<img class="item-img" src={mysql} alt="" />
+						MySQL
+					</div>
+				</div>
+			</section>
+		</>
+	);
+}
